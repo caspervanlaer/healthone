5dd6f36adfb6c0129d2ebeafdd444ce638abeb8a
caspervanlaer <casper@savana.solutions>
2022-02-11 14:55:16 +0100

    ja

--- src/Controller/BezoekerController.php
+++ src/Controller/BezoekerController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category;
 
 class BezoekerController extends AbstractController
 {
@@ -24,5 +25,31 @@ class BezoekerController extends AbstractController
     public function indexAction()
     {
        return $this->render('bezoeker/index.html.twig');
+    }
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categoriesAction()
+    {
+
+        $categories=$this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+       return $this->render('bezoeker/categories.html.twig',['categories'=>$categories]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="category")
+     */
+    public function categoryAction($id)
+    {
+
+        $category=$this->getDoctrine()
+            ->getRepository(Category::class)
+            ->find($id);
+
+       return $this->render('bezoeker/categories.html.twig',['category'=>$category]);
     }
 }
